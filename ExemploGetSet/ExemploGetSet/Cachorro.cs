d56448518decf990064a7081bf562de4400aa540
BarbaraKosela <38699532+BarbaraKosela@users.noreply.github.com>
2018-06-04 17:02:08 -0300

    Adição de repositório
    Início de Set, representando o objetivo como definir a informação;

---
+++ ExemploGetSet/ExemploGetSet/Cachorro.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploGetSet
+{
+    class Cachorro
+    {
+        private string Nome;
+        private string Raca;
+        private double Peso;
+        private double Altura;
+
+
+        // Set serve para acessar outra classe em private, tendo como objetivo definir a informação, guardar determinada informação;
+        // Exemplo: Definir senha - Adrian.SetSenha(123);
+
+
+        public void SetNome(string nome)
+        {
+
+            if (nome.Count() < 3)
+            {
+                throw new Exception("Nome deve conter no mínimo três caracteres");
+            }
+
+            if (nome.Count() > 40)
+            {
+                throw new Exception("Nome deve conter no máximo quarenta caracteres");
+            }
+            Nome = nome;
+        }
+
+        public void SetAltura(double altura)
+        {
+            if (altura <= 0)
+            {
+                throw new Exception("Altura não pode ser negativa");
+            }
+
+
+            if (altura > 2)
+            {
+                throw new Exception("A altura deve ser menor que dois metros");
+            }
+
+            Altura = altura;
+        }
+
+
+        public void SetPeso(double peso)
+        {
+            if (peso <= 0)
+            {
+                throw new Exception("O peso não pode ser negativo");
+            }
+
+            if (peso > 80)
+            {
+                throw new Exception("O peso não pode ultrapassar cinquenta quilos");
+            }
+        }
+
+        public void SetRaca(string raca)
+        {
+            if (raca.Count() < 3)
+            {
+                throw new Exception("A raça deve conter no mínimo três caracteres");
+            }
+
+            if (raca.Count() > 40)
+            {
+                throw new Exception("A raça não pode ultrapassar de quarenta caracteres");
+            }
+        }
+
+    }
+}
---
+++ ExemploGetSet/ExemploGetSet/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploGetSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cachorro dogao = new Cachorro();
+            dogao.SetNome("");
+            dogao.SetAltura(1.10);
+            dogao.SetPeso(80);
+            dogao.SetRaca("");
+        }
+    }
+}
