a8e3465125eb5c1557df9cbe07b69892ebea587d
BarbaraKosela <38699532+BarbaraKosela@users.noreply.github.com>
2018-06-05 15:53:40 -0300

    Adição de exemplo folha
    Fizemos um exercício sem olhar para o programa para ver se lembrava-mos dos comandos para Get e Set, os erros que fiz na folha estão apontados como comentários no programa. Algumas coisas foram esquecidas que de certa forma fazia o código funcionar;

---
+++ ExemploCabeloECasaco/ExemploCabeloECasaco/Cabelo.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploCabeloECasaco
+{
+    class Cabelo
+    {
+            public int TamanhoFio;
+            public double GrossuraFio;
+            public string CorFio;
+
+
+        public void SetTamanhoFio(int tamanhoFio/*tamanhoFio*/)
+        {
+            if (tamanhoFio < 1)
+            
+            {
+                throw new Exception("Tamanho do fio deve ser de pelo menos 1");
+            }
+
+            if (tamanhoFio > 90)
+            
+            {
+                throw new Exception("Tamanho do fio deve ser ao máximo de 90");
+            }
+            TamanhoFio = tamanhoFio;
+        }
+
+        public void SetGrossuraFio(double grossuraFio/*grossuraFio*/)
+        {
+            if (grossuraFio <= 0)
+            {
+               throw new Exception("Grossura do fio deve ser ao mínimo de 0.1");
+
+            }
+
+            if (grossuraFio > 1)
+            {
+                throw new Exception("Grossura do fio deve ser ao máximo de 1");
+            }
+
+            GrossuraFio = grossuraFio;
+        }
+
+        public void SetCorFio(string corFio/*corFio*/)
+        {
+            if (corFio.Count() < 7)
+            {
+                throw new Exception("Cor do fio deve ser ao mínimo de 7 caracteres");
+            }
+
+
+            if (corFio.Count() > 20)
+            {
+                throw new Exception("Cor do fio deve ser ao máximo de 20 caracteres");
+            }
+
+            CorFio = corFio;
+        }
+
+        //Return to... Não feito no papel.
+
+        public /*void*/ int GetTamanhoFio()
+        {
+            return TamanhoFio;
+        }
+
+        public /*void*/ double GetGrossuraFio()
+        {
+            return GrossuraFio;
+        }
+
+        public /*void*/ string GetCorFio()
+        {
+            return CorFio;
+        }
+
+       
+
+    }
+}
---
+++ ExemploCabeloECasaco/ExemploCabeloECasaco/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploCabeloECasaco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Pedaços arrumados de código; 
+            Cabelo cabelo = new Cabelo();
+            /*public string TamanhoFio;
+            public double GrossuraFio;
+            public string CorFio;*/
+
+
+            //parte não feita:
+
+            cabelo.SetTamanhoFio(10); //No caso, a parte do Set foi trocada por Get e o ''cabelo'' não foi colocado.
+            cabelo.SetGrossuraFio(0.12);
+            cabelo.SetCorFio("Vermelinho");
+
+
+            Console.WriteLine(cabelo.GetTamanhoFio());
+            Console.WriteLine(cabelo.GetGrossuraFio());
+            Console.WriteLine(cabelo.GetCorFio());
+            
+        }
+    }
+}
